b38f1f580fffad7b5a626a49ffd0fad770c642d2
Seyyed Amin <seyyed.amin.8809@gmail.com>
2025-03-04 11:37:12 +0330

    jump
    Add jump simple answer

--- 009.Jump Game.py
+++ 009.Jump Game.py
@@ -1,8 +1,23 @@
 nums = [2,3,1,1,4]
-#nums = [3,2,1,0,4]
+# nums = [2,0]
 
 def jump(nums):
+    n = len(nums)
+    location = 0
     if len(nums) == 1:
         return True
-    elif len(nums) >= 2:
-        
+    
+    elif n >= 2:
+        while True:
+            try:
+                if location == n - 1:
+                    return True
+                if nums[location] == 0 and location != n - 1:
+                    return False
+                
+                location += nums[location]
+            except:
+                return False
+            
+
+print(jump(nums))
