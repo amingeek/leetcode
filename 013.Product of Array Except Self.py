91fa0281128fb88e734a17701a48de3d69c6d0d5
Seyyed Amin <seyyed.amin.8809@gmail.com>
2025-04-20 21:38:46 +0330

    Update 013.Product of Array Except Self.py

--- 013.Product of Array Except Self.py
+++ 013.Product of Array Except Self.py
@@ -1,1 +1,19 @@
-nums = [5,3,2,8,6,1]
+nums = [1,2,3,4]
+
+def pro(nums):
+    n = len(nums)
+    prefix = [1] * n
+    suffix = [1] * n
+    answer = [1] * n
+
+    for i in range(1, n):
+        prefix[i] = prefix[i - 1] * nums[i - 1]
+
+    for i in range(n - 2, -1, -1):
+        suffix[i] = suffix[i + 1] * nums[i + 1]
+
+    for i in range(n):
+        answer[i] = prefix[i] * suffix[i]
+
+    return answer
+print(pro(nums))
