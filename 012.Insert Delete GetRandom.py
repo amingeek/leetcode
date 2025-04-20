7cb192c286c28cb94b11dd443519accc01975944
Seyyed Amin <seyyed.amin.8809@gmail.com>
2025-04-20 20:37:03 +0330

    Create 012.Insert Delete GetRandom.py

---
+++ 012.Insert Delete GetRandom.py
@@ -0,0 +1,14 @@
+class RandomizedSet:
+
+    def __init__(self):
+        self.data = []
+        self.indices = []
+        
+    def insert(self, val: int) -> bool:
+        
+
+    def remove(self, val: int) -> bool:
+        
+
+    def getRandom(self) -> int:
+        
